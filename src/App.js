097f5fecd028b703434c188a769d01e5a6bb9343
Brooke Yalof <bsyalof@gmail.com>
2020-07-14 22:43:05 -0400

    modifications

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ function App() {
     return (
       <div className="container flex justify-center fixed top-0 left-0 right-0 bottom-0 p3">
         <div className="flex-main flex flex-column justify-center">
-          <p>I'm Brooke Scarlett Yalof, a full-stack engineer and digital designer based in New York City. Currently, I'm developing a design system <a href="https://github.com/mongodb/leafygreen-ui" {...anchorProps}>@MongoDB</a>.</p>
-          <p>I'm interested in building beautiful interfaces for users, and creating a more inclusive Internet.</p>
+          <p>I'm Brooke Scarlett Yalof, a full-stack engineer and digital designer. Currently, I'm developing a design system <a href="https://github.com/mongodb/leafygreen-ui" {...anchorProps}>@MongoDB</a>. I'm passionate about building beautiful interfaces for users and creating a more inclusive Internet.</p>
           
           <p>
           <span className="icon" role="img" aria-label="pile of books">📚</span>The Brother's Karamazov, Giovanni's Room, A Moveable Feast, <a href="https://www.theinfatuation.com/new-york" {...anchorProps}>The Infatuation </a>
