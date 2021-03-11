af867a46f2ed0787390a4b14b6283cf064a047d4
Brooke Yalof <bsyalof@gmail.com>
2021-03-11 16:38:51 -0500

    update books

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           <p>I'm Brooke Scarlett Yalof, a full-stack engineer and digital designer. Currently, I'm developing a design system <a href="https://github.com/mongodb/leafygreen-ui" {...anchorProps}>@MongoDB</a>. I'm passionate about building beautiful interfaces for users and creating a more inclusive Internet.</p>
           
           <p>
-          <span className="icon" role="img" aria-label="pile of books">📚</span>The Brother's Karamazov, Giovanni's Room, A Moveable Feast, <a href="https://www.theinfatuation.com/new-york" {...anchorProps}>The Infatuation </a>
+          <span className="icon" role="img" aria-label="pile of books">📚</span>The Brother's Karamazov, The Argonauts, A Moveable Feast, <a href="https://www.theinfatuation.com/new-york" {...anchorProps}>The Infatuation </a>
           </p>
 
           <p>
@@ -27,6 +27,7 @@ function App() {
           <p>
             <a className="mr1" href="https://github.com/brookescarlett" {...anchorProps}>Github</a>
             <a className="mr1" href="https://medium.com/@brookeyalof2017" {...anchorProps}>Medium</a>
+            <a className="mr1" href="https://www.linkedin.com/in/brooke-yalof/" {...anchorProps}>LinkedIn</a>
             <a className="mr1" href="mailto:bsyalof@gmail.com">Email</a>
           </p>
         </div>
