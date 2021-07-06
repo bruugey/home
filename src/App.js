eea1bd8fa224843717d3e5d6a6a4d33fde51b120
Brooke Yalof <bsyalof@gmail.com>
2021-07-06 18:35:15 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-
   const anchorProps = {
-    target: "_blank", 
+    target: "_blank",
     rel: "noopener noreferrerr"
-  }
+  };
 
-    return (
-      <div className="container flex justify-center fixed top-0 left-0 right-0 bottom-0 p3">
-        <div className="flex-main flex flex-column justify-center">
-          <h3>Hello there,</h3>
+  return (
+    <div className="fixed top-0 left-0 bottom-0 right-0 flex flex-column justify-center container px4">
+      <p>
+        Hi, I'm <span className="bold">Brooke Scarlett Yalof</span>. I am a
+        user-first software engineer, based in New York City, who is passionate
+        about building accessible, purposeful and inclusive experiences on the
+        web. Currently, I am working on the Dashboard Experience at{" "}
+        <a href="https://stripe.com/" {...anchorProps}>
+          Stripe
+        </a>
+        . Previously, I built a{" "}
+        <a href="https://github.com/mongodb/leafygreen-ui" {...anchorProps}>
+          Design System at MongoDB
+        </a>
+        .
+      </p>
 
-          <p>I'm Brooke Scarlett Yalof, a full-stack engineer based in New York City. Currently, I'm developing an accessible design system <a href="https://github.com/mongodb/leafygreen-ui" {...anchorProps}>@MongoDB</a>. I'm passionate about building seamless experiences for developers, beautiful interfaces for users, and an inclusive Internet for everyone.</p>
-        
-          <p>
-          <span className="icon" role="img" aria-label="pile of books">📚</span>The Brother's Karamazov, The Argonauts, A Moveable Feast, <a href="https://www.theinfatuation.com/new-york" {...anchorProps}>The Infatuation</a>
-          </p>
+      <p>
+        Reach out if you want to talk about web accessibility, (queer)
+        literature, or where to see live music (mostly Grateful Dead cover
+        bands).
+      </p>
 
-          <p>
-            <span className="icon" role="img" aria-label="globe">🌎</span>Yoga, <a href="https://outintech.com/" {...anchorProps}>Out In Tech</a>, Cooking, Traveling, Film
-          </p>
-
-          
-           
-          <p>
-            <a className="mr1" href="https://github.com/brookescarlett" {...anchorProps}>Github</a>
-            <a className="mr1" href="https://medium.com/@brookeyalof2017" {...anchorProps}>Medium</a>
-            <a className="mr1" href="https://www.linkedin.com/in/brooke-yalof/" {...anchorProps}>LinkedIn</a>
-            <a className="mr1" href="mailto:bsyalof@gmail.com">Email</a>
-          </p>
-        </div>
-        <div className="flex-minor flex justify-end">
-          <span className='yin-yang'></span>
-        </div>
-      </div>
-    );
-
+      <p>
+        <span role="img" aria-label="call me emoji">
+          🤙
+        </span>
+        <a href="https://github.com/brookescarlett" {...anchorProps}>
+          Github
+        </a>
+        ,{" "}
+        <a href="mailto:bsyalof@gmail.com" {...anchorProps}>
+          Email
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default App;
